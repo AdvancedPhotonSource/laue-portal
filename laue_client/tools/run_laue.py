6f56a929c73e01cf8aab6fec2a4a108d43d8ec59
linked-liszt <anoxymoren@gmail.com>
2023-02-02 18:37:31 -0600

    Default to preempt script

--- laue_client/tools/run_laue.py
+++ laue_client/tools/run_laue.py
@@ -2,7 +2,7 @@ from gladier import GladierBaseTool, generate_flow_definition
 
 def qsub_launch(**data) -> int:
     import subprocess
-    proc_data = subprocess.call(['/usr/bin/bash', 'funcx_launch/launch_scripts/gladier_debug.sh', data['im_dir'], data['out_dir']]) 
+    proc_data = subprocess.call(['/usr/bin/bash', 'funcx_launch/launch_scripts/gladier_preempt.sh', data['im_dir'], data['out_dir']]) 
     return proc_data
 
 @generate_flow_definition(modifiers={
