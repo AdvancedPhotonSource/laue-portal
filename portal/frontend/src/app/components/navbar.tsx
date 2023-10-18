46dcf12847f882bdeb35dfcc490755d4ba9fb630
Sector 34 group account <epix34id@hpcs34.xray.aps.anl.gov>
2023-10-18 14:14:13 -0500

    Add early daisyui experimentation

---
+++ portal/frontend/src/app/components/navbar.tsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Laue Microdiffraction Portal</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Scans</a></li>
+                    <li><a>Running Reconstructions</a></li>
+                    <li><a>Results</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
