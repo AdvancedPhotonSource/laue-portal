0e2c4c01e4c641a8f392ff3724f38d46b5f4c068
linked-liszt <anoxymoren@gmail.com>
2023-10-24 12:00:24 -0500

    Working dyanmic table

--- portal/frontend/src/app/scans/page.tsx
+++ portal/frontend/src/app/scans/page.tsx
@@ -1,43 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Scans() {
+
+  const [scans, setScans] = useState([]);
+
+  useEffect(() => {
+    const fetchata = async () => {
+
+      const response = await fetch(
+        'api/sample_scans');
+      const data = await response.json();
+      
+      setScans(data)
+    }
+    // Call the function
+    fetchata();
+  }, []);
+
+
+
   return (
-      <div className="drawer-content flex flex-col justify-start overflow-x-auto">
-          <table className="table table-sm table-pin-cols">
-            {/* head */}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Scans</th>
-                <th>Scans2</th>
-                <th>scans3</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-              </tr>
-            </tbody>
-          </table>
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+    <div>
+      <div className="flow-root">
+        <h1 className="card-title float-left">Scans</h1>
+        <input type="text" placeholder="Search" className="input input-sm input-bordered float-right" />
       </div>
+      <div className="divider"></div>
+      <table className="table table-pin-cols table-zebra">
+        <thead>
+          <tr>
+            {
+              scans.headers?.map((header, key) =>
+              <th key={key}>{header}</th>
+              )
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            scans.data?.map((scan, key) =>
+              <tr key={key}>
+                {
+                  scans.headers?.map((header, key) => 
+                    <td key={key}>{scan[header]}</td>
+                  )
+                }
 
+              </tr>
+            )
+          }
+
+        </tbody>
+      </table>
+    </div>
   )
 }
