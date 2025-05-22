7a2f8c7cbceff8998730fb43069f5425b5c0ef68
linked-liszt <anoxymoren@gmail.com>
2025-05-22 11:29:06 -0500

    Move new scans into manual entry dropdown

--- laue_portal/components/navbar.py
+++ laue_portal/components/navbar.py
@@ -3,14 +3,22 @@ import dash_bootstrap_components as dbc
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("Scans", href="/", active="exact")),
-        dbc.NavItem(dbc.NavLink("New Scan", href="/create-scan", active="exact")),
         dbc.NavItem(dbc.NavLink("Reconstructions", href="/reconstructions", active="exact")),
-        dbc.NavItem(dbc.NavLink("New Reconstruction", href="/create-reconstruction", active="exact")),
         dbc.NavItem(dbc.NavLink("Indexations", href="/indexedpeaks", active="exact")),
-        dbc.NavItem(dbc.NavLink("New Indexation", href="/create-indexedpeaks", active="exact")),
         dbc.NavItem(dbc.NavLink("Run Monitor", href="/index-runs", active="exact")),
+        dbc.DropdownMenu(
+            id="manual-entry-dropdown",
+            children=[
+                dbc.DropdownMenuItem("New Scan", href="/create-scan"),
+                dbc.DropdownMenuItem("New Reconstruction", href="/create-reconstruction"),
+                dbc.DropdownMenuItem("New Indexation", href="/create-indexedpeaks"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Manual Entry",
+        ),
     ],
-    brand="Coded Aperture Laue",
+    brand="3DMN Portal",
     brand_href="/",
     color="primary",
     className="navbar-lg",
